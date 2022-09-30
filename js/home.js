7de14b310407b13036ad1f557f347c9cb16138cb
Innocent-Dlamini-3080 <tycoon3080@gmail.com>
2022-10-01 00:26:30 +0200

    initial commit

---
+++ js/home.js
@@ -0,0 +1,12 @@
+function signUp(){
+	alert("Phone: +254 777 888 333");
+}
+
+function openSignUp(){
+		window.location.replace("sign-up.html");
+}
+
+function closeDoc(){
+window.location.replace("home.html");
+}
+
